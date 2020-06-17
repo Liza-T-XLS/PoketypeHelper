5d275d66e48406b2bdea90d4c64ae35235e58ec4
Sha <shapi94@gmail.com>
2020-06-17 12:58:58 +0200

    box-shadow bug fix

--- public/js/app.js
+++ public/js/app.js
@@ -5,7 +5,7 @@ const app = {
 
     init: function() {
         console.log('init');
-        console.log(app.buttons);
+
         for (button of app.buttons) {
             button.addEventListener('click', app.buttonsHandler);
         };
@@ -13,38 +13,31 @@ const app = {
     },
 
     buttonsHandler: function(e) {
-        console.log('clicked');
-        console.log(e.target);
+
         var target = e.target;
 
         if(target.matches('.active')) {
-            console.log('active');
             document.getElementById('weaknesses').style.display = 'none';
             target.classList.remove('active');
-            target.style.boxShadow = 'none';
             return true;
         };
         
         document.getElementById('weaknesses').style.display = 'block';
-        
 
         for (button of app.buttons) {
             button.classList.remove('active');
-            button.style.boxShadow = 'none';
         };
+
         for (image of app.images) {
             image.classList.remove('selected');
         };
-        target.style.boxShadow = '5px 5px 15px #555';
+
         target.classList.add('active');
-        console.log(e.target.dataset.typeName);
         var typeName = e.target.dataset.typeName;
         var image = document.getElementById(typeName);
         image.classList.add('selected');
 
-        console.log(e.target.dataset.typeWeaknesses);
         var weaknesses = JSON.parse(e.target.dataset.typeWeaknesses)
-        console.log(weaknesses);
         var list = document.getElementById('typeList');
         list.innerHTML = '';
         var choice = document.getElementById('choice');
@@ -63,7 +56,7 @@ const app = {
         }
 
         if (window.matchMedia("(max-width: 425px)").matches) {
-            console.log('small indeed');
+            
             var weaknessesDiv = document.getElementById('weaknesses');
             target.after(weaknessesDiv);
         }
@@ -73,16 +66,12 @@ const app = {
     resizeHandler: function(e) {
         if (window.matchMedia("(min-width: 426px)").matches) {
             target = document.getElementById('types');
-            console.log(target);
             var weaknessesDiv = document.getElementById('weaknesses');
-            console.log(target.parentNode);
             target.after(weaknessesDiv);
         }
         if (window.matchMedia("(max-width: 425px)").matches) {
             target = document.querySelector('.active');
-            console.log(target);
             var weaknessesDiv = document.getElementById('weaknesses');
-            console.log(target.parentNode);
             target.after(weaknessesDiv);
         }
     }
